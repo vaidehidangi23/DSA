044277f8ebec9a03d5ae0b23bbf1c9254392a671
vaidehidangi23 <62616333+vaidehidangi23@users.noreply.github.com>
2023-03-06 18:06:41 +0530

    first non-repeating character in a stream

---
+++ Queue/1stNonRepeatingCharater.cpp
@@ -0,0 +1,32 @@
+class Solution {
+	public:
+		string FirstNonRepeating(string A){
+		    // Code here
+            map<char, int> count;
+		    string ans = "";
+		    queue<char> q;
+		    
+		    for(int i=0; i<A.length(); i++) {
+		        char ch = A[i];
+		        
+		        q.push(ch);
+		        count[ch]++;
+		        
+		        while(!q.empty()) {
+		            if(count[q.front()] > 1){
+		                q.pop();
+		            }
+		            else
+		            {
+		                ans.push_back(q.front());
+		                break;
+		            }
+		        }
+		        if(q.empty()){
+		            ans.push_back('#');
+		        }
+		    }
+		    return ans;
+		}
+
+};
